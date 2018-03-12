2efdac8e9196839cd56cf8ff1a19f2e5626572ff
kyisea <ky_199@163.com>
2018-03-12 20:38:23 +0800

    del

--- src/cn/itcast/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package cn.itcast.constant;
-
-public class Constant {
-    public static final String PRODUCT_NAME = "product_name";
-    public static final String PRODUCT_NAME2 = "product_name2";
-}
