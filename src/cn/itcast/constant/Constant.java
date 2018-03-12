ddc2ce0690bc7b99f9a69b00aed970b25fcfc4ef
kyisea <ky_199@163.com>
2018-03-12 20:28:09 +0800

    新增源文件

---
+++ src/cn/itcast/constant/Constant.java
@@ -0,0 +1,6 @@
+package cn.itcast.constant;
+
+public class Constant {
+    public static final String PRODUCT_NAME = "product_name";
+    public static final String PRODUCT_NAME2 = "product_name2";
+}
